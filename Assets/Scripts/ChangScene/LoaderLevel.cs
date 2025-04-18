18a9e95636dc2375ef85f8413bb36b1a6c2c3da6
Mathis <mathis.buclon@efrei.net>
2025-04-18 19:38:58 +0200

    Avanced on Procedural Generation

--- Assets/Scripts/ChangScene/LoaderLevel.cs
+++ Assets/Scripts/ChangScene/LoaderLevel.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class LoaderLevel : MonoBehaviour
 {
+    // Salles
+    public GameObject[] roomPrefabs;
+    private GameObject[] generatedRooms;
+    public GameObject spawnRoom;
+    public GameObject portalRoom;
+    public GameObject MurVertical;
+    public GameObject MurHorizontal;
+    // Compteurs de Salles
+    public int nbFightRoomsAllowed;
+    private int nbFightRooms;
+    public int nbItemsRoomsAllowed;
+    private int nbItemsRooms;
+    public int nbChessRoomsAllowed;
+    private int nbChessRooms;
+    // Positions
+    public Vector2 currentPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentPos.x = 0;
+        currentPos.y = 0;
+        GenerateRooms();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GenerateRooms()
+    {
+        GameObject room = Instantiate(spawnRoom, currentPos, Quaternion.identity);
+        //while (nbChessRooms <= nbChessRoomsAllowed || nbFightRooms <= nbFightRoomsAllowed || nbItemsRooms < nbItemsRoomsAllowed)
+        //{
+
+        //} 
+
     }
 }
