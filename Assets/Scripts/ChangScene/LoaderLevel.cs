31740ebd54b7943cf1190d6b6591425409a5dbda
mathis <mathis.buclon@efrei.net>
2025-04-19 15:03:49 +0200

    Smal start of procedural generation

--- Assets/Scripts/ChangScene/LoaderLevel.cs
+++ Assets/Scripts/ChangScene/LoaderLevel.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LoaderLevel : MonoBehaviour
 {
     // Salles
-    public GameObject[] roomPrefabs;
+    public GameObject[] fightRoomPrefabs;
+    public GameObject[] itemsRoomPrefabs;
+    public GameObject[] chessRoomPrefabs;
     private GameObject[] generatedRooms;
     public GameObject spawnRoom;
     public GameObject portalRoom;
@@ -21,11 +24,13 @@ public class LoaderLevel : MonoBehaviour
     // Positions
     public Vector2 currentPos;
 
+    // Offset entre chaque salle (ex : 20x20 unités)
+    public Vector2 roomOffset = new Vector2(20, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-        currentPos.x = 0;
-        currentPos.y = 0;
+        currentPos = Vector2.zero;
         GenerateRooms();
     }
 
@@ -37,11 +42,51 @@ public class LoaderLevel : MonoBehaviour
 
     public void GenerateRooms()
     {
-        GameObject room = Instantiate(spawnRoom, currentPos, Quaternion.identity);
-        //while (nbChessRooms <= nbChessRoomsAllowed || nbFightRooms <= nbFightRoomsAllowed || nbItemsRooms < nbItemsRoomsAllowed)
-        //{
+        Instantiate(spawnRoom, currentPos, Quaternion.identity);
 
-        //} 
+        // Avancer la position pour la prochaine salle
+        currentPos += roomOffset;
 
+        // Génération jusqu’à atteindre le nombre max de chaque type de salle
+        while (nbFightRooms < nbFightRoomsAllowed || nbItemsRooms < nbItemsRoomsAllowed || nbChessRooms < nbChessRoomsAllowed)
+        {
+            // Choisir une salle au hasard
+            GameObject selectedRoom = null;
+
+            // Choisir un type en fonction des salles encore disponibles
+            int type = Random.Range(0, 3); // 0 = fight, 1 = item, 2 = chess
+
+            if (type == 0 && nbFightRooms < nbFightRoomsAllowed)
+            {
+                int nb = Random.Range(0, fightRoomPrefabs.Length);
+                selectedRoom = fightRoomPrefabs[nb]; // fight
+                nbFightRooms++;
+            }
+            else if (type == 1 && nbItemsRooms < nbItemsRoomsAllowed)
+            {
+                int nb = Random.Range(0, itemsRoomPrefabs.Length);
+                selectedRoom = itemsRoomPrefabs[nb]; // item
+                nbItemsRooms++;
+            }
+            else if (type == 2 && nbChessRooms < nbChessRoomsAllowed)
+            {
+                int nb = Random.Range(0, chessRoomPrefabs.Length);
+                selectedRoom = chessRoomPrefabs[nb]; // chess
+                nbChessRooms++;
+            }
+            else
+            {
+                continue; // retry avec une autre salle
+            }
+
+            // Placer la salle
+            Instantiate(selectedRoom, currentPos, Quaternion.identity);
+            currentPos += roomOffset; // avancer
+        }
+
+        // Placer la salle de fin (portal)
+        Instantiate(portalRoom, currentPos, Quaternion.identity);
     }
+
 }
+
